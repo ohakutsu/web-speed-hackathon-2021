560b893c2fc6b1d54f89c76c72a783e614dadfe4
ohakutsu <dev@ohakutsu.com>
2022-04-23 22:50:34 +0900

    Add function of generating sound svg when uploading sound

--- server/src/routes/api/sound.js
+++ server/src/routes/api/sound.js
@@ -8,6 +8,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { convertSound } from '../../converters/convert_sound';
 import { UPLOAD_PATH } from '../../paths';
 import { extractMetadataFromSound } from '../../utils/extract_metadata_from_sound';
+import { generateSoundSvgFromBuffer } from '../../utils/generate-sound-svg-from-buffer';
 
 // 変換した音声の拡張子
 const EXTENSION = 'mp3';
@@ -33,6 +34,11 @@ router.post('/sounds', async (req, res) => {
 
   const filePath = path.resolve(UPLOAD_PATH, `./sounds/${soundId}.${EXTENSION}`);
   await fs.writeFile(filePath, converted);
+
+  // Generate svg of sound
+  const svg = await generateSoundSvgFromBuffer(converted);
+  const svgFilePath = path.resolve(UPLOAD_PATH, `./sound_svgs/${soundId}.svg`);
+  await fs.writeFile(svgFilePath, svg);
 
   return res.status(200).type('application/json').send({ artist, id: soundId, title });
 });
