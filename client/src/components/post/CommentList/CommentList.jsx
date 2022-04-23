aeb2135182bae9e3d75a052a2098c792cd66f459
ohakutsu <dev@ohakutsu.com>
2022-04-23 15:19:58 +0900

    Pass lastItemRef to PostPage

--- client/src/components/post/CommentList/CommentList.jsx
+++ client/src/components/post/CommentList/CommentList.jsx
@@ -5,14 +5,18 @@ import { CommentItem } from '../CommentItem';
 /**
  * @typedef {object} Props
  * @property {Array<Models.Comment>} comments
+ * @property {React.RefObject} lastItemRef
  */
 
 /** @type {React.VFC<Props>} */
-const CommentList = ({ comments }) => {
+const CommentList = ({ comments, lastItemRef }) => {
+  const lastItemIndex = comments.length - 1;
   return (
     <div>
-      {comments.map((comment) => {
-        return <CommentItem key={comment.id} comment={comment} />;
+      {comments.map((comment, index) => {
+        return (
+          <CommentItem key={comment.id} comment={comment} ref={index === lastItemIndex ? lastItemRef : undefined} />
+        );
       })}
     </div>
   );
