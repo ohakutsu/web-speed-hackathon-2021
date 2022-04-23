7211217777f4a3a6b415b71f62d911839bc1746b
ohakutsu <dev@ohakutsu.com>
2022-04-23 22:49:38 +0900

    Use generated svg images instead of Web Audio API

--- client/src/components/foundation/SoundWaveSVG/SoundWaveSVG.jsx
+++ client/src/components/foundation/SoundWaveSVG/SoundWaveSVG.jsx
@@ -1,67 +1,18 @@
-import lodashChunk from 'lodash/chunk';
-import lodashMap from 'lodash/map';
-import lodashMax from 'lodash/max';
-import lodashMean from 'lodash/mean';
-import lodashZip from 'lodash/zip';
 import React from 'react';
-
-/**
- * @param {ArrayBuffer} data
- * @returns {Promise<{ max: number, peaks: number[] }}
- */
-async function calculate(data) {
-  const audioCtx = new AudioContext();
-
-  // 音声をデコードする
-  /** @type {AudioBuffer} */
-  const buffer = await new Promise((resolve, reject) => {
-    audioCtx.decodeAudioData(data.slice(0), resolve, reject);
-  });
-  // 左の音声データの絶対値を取る
-  const leftData = lodashMap(buffer.getChannelData(0), Math.abs);
-  // 右の音声データの絶対値を取る
-  const rightData = lodashMap(buffer.getChannelData(1), Math.abs);
-
-  // 左右の音声データの平均を取る
-  const normalized = lodashMap(lodashZip(leftData, rightData), lodashMean);
-  // 100 個の chunk に分ける
-  const chunks = lodashChunk(normalized, Math.ceil(normalized.length / 100));
-  // chunk ごとに平均を取る
-  const peaks = lodashMap(chunks, lodashMean);
-  // chunk の平均の中から最大値を取る
-  const max = lodashMax(peaks);
-
-  return { max, peaks };
-}
+import { getSoundSvgPath } from '../../../utils/get_path';
 
 /**
  * @typedef {object} Props
- * @property {ArrayBuffer} soundData
+ * @property {number} soundId
  */
 
 /**
  * @type {React.VFC<Props>}
  */
-const SoundWaveSVG = ({ soundData }) => {
-  const uniqueIdRef = React.useRef(Math.random().toString(16));
-  const [{ max, peaks }, setPeaks] = React.useState({ max: 0, peaks: [] });
+const SoundWaveSVG = ({ soundId }) => {
+  const soundSvgPath = getSoundSvgPath(soundId);
 
-  React.useEffect(() => {
-    calculate(soundData).then(({ max, peaks }) => {
-      setPeaks({ max, peaks });
-    });
-  }, [soundData]);
-
-  return (
-    <svg className="w-full h-full" preserveAspectRatio="none" viewBox="0 0 100 1">
-      {peaks.map((peak, idx) => {
-        const ratio = peak / max;
-        return (
-          <rect key={`${uniqueIdRef.current}#${idx}`} fill="#2563EB" height={ratio} width="1" x={idx} y={1 - ratio} />
-        );
-      })}
-    </svg>
-  );
+  return <img className="w-full h-full" src={soundSvgPath} loading="lazy" />;
 };
 
 export { SoundWaveSVG };
