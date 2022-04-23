79791876e3292c8097219ed9cba69d2e8eb0f1f0
ohakutsu <dev@ohakutsu.com>
2022-04-24 03:41:03 +0900

    Use webp instead of jpg

--- client/src/utils/get_path.js
+++ client/src/utils/get_path.js
@@ -3,7 +3,7 @@
  * @returns {string}
  */
 function getImagePath(imageId) {
-  return `/images/${imageId}.jpg`;
+  return `/images/${imageId}.webp`;
 }
 
 /**
@@ -35,7 +35,7 @@ function getSoundSvgPath(soundId) {
  * @returns {string}
  */
 function getProfileImagePath(profileImageId) {
-  return `/images/profiles/${profileImageId}.jpg`;
+  return `/images/profiles/${profileImageId}.webp`;
 }
 
 export { getImagePath, getMoviePath, getSoundPath, getSoundSvgPath, getProfileImagePath };
