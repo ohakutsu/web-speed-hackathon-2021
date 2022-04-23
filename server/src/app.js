84e74d6d5002e5e1337507f1cc4bccbf43d0d1f5
ohakutsu <dev@ohakutsu.com>
2022-04-23 12:29:30 +0900

    Add compression

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 import bodyParser from 'body-parser';
+import compression from 'compression';
 import Express from 'express';
 import session from 'express-session';
-
 import { apiRouter } from './routes/api';
 import { staticRouter } from './routes/static';
 
@@ -22,11 +22,12 @@ app.use(bodyParser.raw({ limit: '10mb' }));
 
 app.use((_req, res, next) => {
   res.header({
-    'Cache-Control': 'max-age=0, no-transform',
+    'Cache-Control': 'max-age=0',
     Connection: 'close',
   });
   return next();
 });
+app.use(compression());
 
 app.use('/api/v1', apiRouter);
 app.use(staticRouter);
